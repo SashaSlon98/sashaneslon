f4e83db6cfb959fc6fc82df29708ff159036463c
SashaSlon <sashaslon1998@gmail.com>
2017-02-14 00:21:11 +0300

    Create var3.py

---
+++ hw_15/var3.py
@@ -0,0 +1,17 @@
+import re
+b = input ('Название статьи: ')
+f = open(b, 'r', encoding = 'utf-8')
+q = input ("Новая статья: ")
+l = open(p, 'w', encoding = 'utf-8')
+
+m = f.readlines()
+for elmnt in m:
+    elmnt = re.split(' ', elmnt)
+    elString = ""
+    for word in elmnt:
+        word = re.sub(u'язык', u'шашлык', word)
+        word = re.sub(u'Язык', u'Шашлык', word)
+        elString += word + ' '
+    elmnt = elString
+    print(elmnt)
+    h.write(elmnt)
