a446c3eaca787c596b9c975d4e2a52013a50fc4d
SashaSlon <sashaslon1998@gmail.com>
2017-06-22 19:37:18 +0300

    Update and rename 5 ballov to variant.py

---
+++ trenirovka exam/variant.py
@@ -0,0 +1,42 @@
+import os
+import re
+
+def fun_1():
+    f = open(r'text.xml', 'r', encoding="utf-8")
+    l = 0
+    ana = 0
+    for r in f:
+        if 'w' in r:
+            l += 1
+            ana += r.count('ana')
+            
+    print('Среднее количество разборов = {}'.format(ana/l))
+
+
+def fun_2():
+    f = open(r'text.xml', 'r', encoding="utf-8")
+    
+    d = {'S':0, 'A':0, 'NUM':0, 'ANUM':0, 'V':0, 'ADV':0, 'PREDIC':0,
+         'PARENTH':0, 'SPRO':0, 'APRO':0, 'ADVPRO':0, 'PREDICPRO':0, 'PR':0,
+         'CONJ':0, 'PART':0, 'INTJ':0}
+    
+    for r in f:
+        if 'w' in r:
+            for k in d.keys():
+                if k in r:
+                    d[k]+=1
+    o = open('text_dict.txt', 'w')
+    for k in d.keys():
+        o.write('{}\t{}\n'.format(k, d[k]))
+    print('Файл частотности создан и сохранен в папке, где хранится программа')
+
+def main():
+    
+    fun_1()
+
+    fun_2()
+
+if __name__ == '__main__':
+    main()
+        
+
